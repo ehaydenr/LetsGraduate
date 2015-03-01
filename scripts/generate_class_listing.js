db24371d60681dc4a0729205b0830e90e1942bd6
Eric Rosenberg <ehaydenr@gmail.com>
2015-02-28 18:51:13 -0600

    Class file

---
+++ scripts/generate_class_listing.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+/* MySQL Setup */
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  database : 'letsgraduate_dev',
+  user     : 'dev',
+  password : 'dev'
+});
+
+/* MySQL test connection */
+connection.connect(function(err){
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+});
+
+var data;
+
+var query = 'SELECT department, number, title, description FROM Class;';
+
+connection.query(query, function (err, rows, fields) {
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  // Close connection to DB
+  connection.end();
+
+  // Write data to file
+  fs.writeFile("../public/class.json", JSON.stringify(rows), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("The file was saved!");
+    }
+  }); 
+
+});
+
