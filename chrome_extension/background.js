85063fc7376ac3d2a7de71fe36bd2984c99b6081
Eric Rosenberg <ehaydenr@gmail.com>
2015-04-10 15:50:43 -0500

    Updated Manifest and added more output for background.js

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -9,13 +9,15 @@ chrome.browserAction.onClicked.addListener( function(request, sender, sendRespon
   function (result) {
     console.log(result);
     chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+      console.log(token);
       var url = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + token;
       $.get( url, function( data ) {
         var id = data.user_id;
+        console.log("id: " + id);
         url = server_url + "/import";
         $.post( url, { "id": id, "data": JSON.stringify(result) }, "json")
         .done(function( data ) {
-          console.log(data);
+          //console.log(data);
         });
       });
     });
