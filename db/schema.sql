f2d1af3d3078cc255a1f9102034c6af162868630
Eric Rosenberg <ehaydenr@gmail.com>
2015-04-22 17:54:57 -0500

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.22, for osx10.10 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.23, for osx10.10 (x86_64)
 --
 -- Host: localhost    Database: letsgraduate_dev
 -- ------------------------------------------------------
--- Server version	5.6.22
+-- Server version	5.6.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -147,4 +147,4 @@ CREATE TABLE `UserClass` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-22  0:58:19
+-- Dump completed on 2015-04-22 17:54:44
