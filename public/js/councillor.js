e42a6a8b4a4f2f93dfeede503e8ac56666c0d8a7
Eric Rosenberg <ehaydenr@gmail.com>
2015-04-22 02:02:46 -0500

    Display sections

--- public/js/councillor.js
+++ public/js/councillor.js
@@ -2,10 +2,20 @@ $(document).ready(function () {
   function addClassToProspective(id){
     console.log("addClassToProspective: " + id);
   }
+  function populateColumns(types, key, id){
+    $('#header' + (id + 1))[0].innerHTML = key;
+    for(var i = 0; i < types[key].length; ++i){
+      var div = document.createElement('div');
+      div.className =  'col-md-12';
+      div.innerHTML = types[key][i].name;
+      document.getElementById('header' + (id + 1)).appendChild(div);
+    }
+  }
   function exploreProspective(id){
     console.log("exploreProspective: " + id);
     $.get( "/class/" + id + "/json", function( data ) {
       var section_data = data.section_data;
+      console.log(section_data);
       var types = {};
       for(var i = 0; i < section_data.length; ++i){
         types[section_data[i].type] = [];
@@ -14,15 +24,18 @@ $(document).ready(function () {
         types[section_data[i].type].push(section_data[i]);
       }
       console.log(types);
+      document.getElementById('header1').innerHTML = '';
+      document.getElementById('header2').innerHTML = '';
+      document.getElementById('header3').innerHTML = '';
       var keys = Object.keys(types);
       if(keys.length >= 1){
-        $('#header1')[0].innerHTML = keys[0];
+        populateColumns(types, keys[0], 0);
       }
       if(keys.length >= 2){
-        $('#header2')[0].innerHTML = keys[1];
+        populateColumns(types, keys[1], 1);
       }
       if(keys.length >= 3){
-        $('#header3')[0].innerHTML = keys[2];
+        populateColumns(types, keys[2], 2);
       }
     });
   }
